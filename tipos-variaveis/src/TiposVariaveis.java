cd650d5fc8be1e75f27ebdee6c4f56862ea6a7b0
Rafael Rodrigues <rafaelr.osteam@gmail.com>
2025-03-18 17:52:12 -0300

    Trabalhando com variáveis e constantes.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        String BR = "Brasil";
+        double PI = 3.14;
+        int ESTADOS_BRASILEIROS = 27;
+        int ANO_2000 = 2000;
+
+        System.out.println(BR);
+        System.out.println(PI);
+        System.out.println(ESTADOS_BRASILEIROS);
+        System.out.println(ANO_2000);
+        
     }
 }
---
+++ tipos-variaveis/src/TiposVariaveis2.java
@@ -0,0 +1,11 @@
+public class TiposVariaveis2 {
+    public static void main(String[] args) {
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; // Se começar com zero, tavelz tenha que ser outro tipo
+        long cpf = 12332112323L; // Se começar com zero, tavelz tenha que ser outro tipo
+        float pi = 3.14F; 
+        double salario = 1275.33;
+
+    }
+}
